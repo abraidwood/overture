b2973cd4d0daff23f0de48fecfa7364628d63ca7
Alistair Braidwood <alistair_braidwood@yahoo.co.uk>
2013-04-29 07:31:29 +0100

    update to same sources and versions for node tests

--- perf/node-test-config.js
+++ perf/node-test-config.js
@@ -20,9 +20,10 @@ module.exports = {
 
 	sources: [
 		{name: 'underscore-1.4.4'},
-		{name: 'backbone-1.0.0'},
 		{name: 'q'},
-		{name: 'jquery-1.9.1'},
+		{name: 'backbone-1.0.0'},
+		{name: 'jquery-2.0.0'},
+//		{name: 'jquery-1.9.1'},
 		{name: 'angular-1.0.6'},
 		{name: 'three-r57'}
 	],
