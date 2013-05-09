5484ecb4933799123f1a8c0c2f0cd32f38fb65dc
Alistair Braidwood <alistair_braidwood@yahoo.co.uk>
2013-05-09 07:54:14 +0100

    Remove jquery-1.9.1 source

--- perf/scripts/sources.js
+++ perf/scripts/sources.js
@@ -11,9 +11,7 @@ var sources = [
     new Source('underscore-1.4.4',true),
     new Source('q',true),
     new Source('backbone-1.0.0',true),
-    //new Source('codemirror-3.11',true),
     new Source('jquery-2.0.0',true),
-    new Source('jquery-1.9.1',false),
     new Source('angular-1.0.6',true),
-    new Source('three-r57',true)//,
+    new Source('three-r57',true)
 ];
